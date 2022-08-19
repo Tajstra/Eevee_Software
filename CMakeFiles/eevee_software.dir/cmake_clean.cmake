6b4dd19764a3e1a70bfd600a11c105511b1431ff
Tajstra <kondziorgrabarz@gmail.com>
2022-08-19 20:10:50 +0200

    Initial commit of the project

---
+++ CMakeFiles/eevee_software.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eevee_software.dir/test/test.cpp.obj"
+  "CMakeFiles/eevee_software.dir/test/test.cpp.obj.d"
+  "eevee_software.exe"
+  "eevee_software.exe.manifest"
+  "eevee_software.pdb"
+  "libeevee_software.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eevee_software.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/eevee_software.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for eevee_software.
+# This may be replaced when dependencies are built.
